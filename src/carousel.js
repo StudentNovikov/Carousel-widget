daab9dbae7f4af3d6989fdc60e71526987fcd99d
StudentNovikov <StudentNovikov@gmail.com>
2020-03-05 15:09:17 -0800

    swithed to animated carousel, hardcoded values atm

--- src/carousel.js
+++ src/carousel.js
@@ -1,14 +1,15 @@
-class Carousel{
-  static id = 0;
-  constructor(values, {rootRefId,dimension = 'X',visibleItems = 5, activeItems = 3, scrollSpeed = 1000, scrollPerClick = 1}){
-    try{
-      this.setValues(values);
-      this.setSettings(rootRefId,dimension,visibleItems,activeItems,scrollSpeed,scrollPerClick);
-      this.validateInitParams();
-      this.init();
-    } catch (error) {
-       console.log('Carousel creation is impossible with given attributes');
-    }
+class Carousel {
+  constructor(values, {
+    rootRefId, dimension = 'X', visibleItems = 5, activeItems = 3, scrollSpeed = 1000, scrollPerClick = 1,
+  }) {
+    // try{
+    this.setValues(values);
+    this.setSettings(rootRefId, dimension, visibleItems, activeItems, scrollSpeed, scrollPerClick);
+    this.validateInitParams();
+    this.init();
+    // } catch (error) {
+    //    console.log('Carousel creation is impossible with given attributes');
+    // }
   }
 
   setValues = (values) => {
@@ -17,7 +18,7 @@ class Carousel{
     } else if ((typeof (values)) === 'object') {
       this.values = values;
     } else throw error;
-  };
+  }
 
   setSettings = (rootRefId, dimension, visibleItems, activeItems, scrollSpeed, scrollPerClick) => {
     this.settings = {};
@@ -28,13 +29,13 @@ class Carousel{
     this.settings.scrollSpeed = scrollSpeed;
     this.settings.scrollPerClick = scrollPerClick;
     if (this.initIsOver) {
-      this.updateSliderContainer(dimension);
+      // this.updateSliderContainer(dimension);
       this.draw();
-      this.removeButtons();
-      this.drawButtons();
+      // this.removeButtons();
+      // this.drawButtons();
       this.addClickListeners();
     }
-  };
+  }
 
   validateInitParams = () => {
     if (!this.settings.rootRefId
@@ -45,30 +46,39 @@ class Carousel{
       || this.settings.scrollPerClick < 0) {
       throw new Error();
     }
-  };
+  }
 
-  init(){
+  init = () => {
     this.carouselId = Carousel.id + 1;
     Carousel.id += 1;
-    this.offset = 0;
 
-    this.createSliderContainer();
+    this.translationComplete = true;
+    this.index = 0;
+    this.currTransl = [];
+
+    // this.createSliderContainer();
     this.draw();
-    this.drawButtons();
+
+    for (let i = 0; i < this.values.length; i += 1) {
+      this.currTransl[i] = 0;
+      document.querySelector(`.slider${this.carouselId}`).querySelectorAll('.item')[i]
+        .addEventListener('transitionend', this.transitionCompleted, true);
+    }
+    // this.drawButtons();
     this.addClickListeners();
     this.addSwipeListeners();
     this.initIsOver = true;
   }
 
-  disableButtons = () => {
-    this.instanceRef.querySelector('.next').disabled = true;
-    this.instanceRef.querySelector('.prev').disabled = true;
-  };
+  // disableButtons = () => {
+  //   this.instanceRef.querySelector('.next').disabled = true;
+  //   this.instanceRef.querySelector('.prev').disabled = true;
+  // };
 
-  enableButtons = () => {
-    this.instanceRef.querySelector('.next').disabled = false;
-    this.instanceRef.querySelector('.prev').disabled = false;
-  };
+  // enableButtons = () => {
+  //   this.instanceRef.querySelector('.next').disabled = false;
+  //   this.instanceRef.querySelector('.prev').disabled = false;
+  // };
 
   scroll = (direction) => {
     this.showAnimation();
@@ -86,124 +96,195 @@ class Carousel{
       obj.draw();
       timerId = setTimeout(displayStep, timeToScroll);
     }, timeToScroll);
-  };
+  }
+
+  // scrollNext = () => {
+  //   this.scroll(-1);
+  // };
+
+  // scrollPrevious = () => {
+  //   this.scroll(1);
+  // };
+
+  // showAnimation = () => {
+  //   // const contentList = document.getElementById(this.settings.rootRefId).querySelectorAll('.content');
+  //   // const contentArray = [...contentList];
+  //   // contentArray.forEach((contentDiv) => {
+  //   //   contentDiv.classList.add('animate');
+  //   // });
+  // };
+
+  // formatIndexWithOffset = (indexWithOffset) => (indexWithOffset % this.values.length >= 0
+  //   ? indexWithOffset % this.values.length
+  //   : (indexWithOffset % this.values.length) + this.values.length);
+
+  // isIndexItemActive = (index) => {
+  //   const lastLowInactive = (this.settings.visibleItems - this.settings.activeItems) / 2 - 1;
+  //   const firstHighInactive = (this.settings.visibleItems
+  //      - (this.settings.visibleItems - this.settings.activeItems) / 2);
+  //   return ((index > lastLowInactive) && (index < firstHighInactive));
+  // };
+
+  // getAllElementsMarkup = () => this.values
+  //   .reduce((elementsMarkup, element, index, valuesArray) => {
+  //     if (index < this.settings.visibleItems) {
+  //       return (this.isIndexItemActive(index))
+  //         ? `${elementsMarkup}<div class="content active">
+  //       <img src="${valuesArray[this.formatIndexWithOffset(index + this.offset)].picture}" alt="">
+  //       <h3>${valuesArray[this.formatIndexWithOffset(index + this.offset)].title}</h3>
+  //       <p>${valuesArray[this.formatIndexWithOffset(index + this.offset)].description}</p>
+  //     </div>`
+  //         : `${elementsMarkup}<div class="content">
+  //       <img src="${valuesArray[this.formatIndexWithOffset(index + this.offset)].picture}" alt="">
+  //       <h3>${valuesArray[this.formatIndexWithOffset(index + this.offset)].title}</h3>
+  //       <p>${valuesArray[this.formatIndexWithOffset(index + this.offset)].description}</p>
+  //     </div>`;
+  //     } return elementsMarkup;
+  //   }, '');
+
+  // getButtonsMarkup = () => `<div class="buttons">
+  //   <button class="next">NEXT</button>
+  //   <button class="prev">PREV</button>
+  // </div>`;
+
+  // createSliderContainer() {
+  //   document.getElementById(this.settings.rootRefId).innerHTML = `<div class="slider${this.carouselId}"></div>`;
+  //   // const slider = document.createElement('div');
+  //   // slider.classList.add('slider');
+  //   // slider.classList.add(`slider${this.carouselId}`);
+  //   // if (this.settings.dimension === 'Y') {
+  //   //   slider.classList.add('slider-y');
+  //   // } else {
+  //   //   slider.classList.add('slider-x');
+  //   // }
+  //   // document.getElementById(this.settings.rootRefId).appendChild(slider);
+  // }
+
+  // updateSliderContainer(newOrientation) {
+  //   document.getElementById(this.settings.rootRefId).querySelector('.slider').classList.remove('slider-x', 'slider-y');
+  //   document.getElementById(this.settings.rootRefId).querySelector('.slider').classList.add(`slider-${newOrientation.toLowerCase()}`);
+  // }
+
+  draw = () => {
+    document.getElementById(this.settings.rootRefId).innerHTML += `<div class="container slider${this.carouselId}">
+    <div class="visibleContainer">
+      <div class="leftbox">
+        <div class="rightbox">
+          <div class="activeContainer animate">
+            <div class="item animate">
+              <img src="http://lorempixel.com/200/200/cats/1/" alt="">
+              <h3>CAT #1</h3>
+              <p>This cat is a nice one for sure </p>
+            </div><div class="item animate">
+              <img src="http://lorempixel.com/200/200/cats/2/" alt="">
+              <h3>CAT #2</h3>
+              <p>Not bad at all</p>
+            </div><div class="item animate" >
+              <img src="http://lorempixel.com/200/200/cats/3/" alt="">
+              <h3>CAT #3</h3>
+              <p>Huge and dangerous</p>
+            </div><div class="item animate">
+              <img src="http://lorempixel.com/200/200/cats/4/" alt="">
+              <h3>CAT #5</h3>
+              <p>Black and white, do u like it?</p>
+            </div><div class="item animate">
+              <img src="http://lorempixel.com/200/200/cats/5/" alt="">
+              <h3>CAT #6</h3>
+              <p>Black and white, do u like it?</p>
+            </div><div class="item animate">
+              <img src="https://placekitten.com/250/250" alt="">
+              <h3>CAT #7</h3>
+              <p>Black and white, do u like it?</p>
+            </div><div class="item animate">
+              <img src="https://placebear.com/240/240" alt="">
+              <h3>CAT #8</h3>
+              <p>Black and white, do u like it?</p>
+            </div>
+          </div>
+        </div>
+     </div>
+    </div>
+    <div class="buttons active">
+      <button class="prev" id="prevSmooth">PREV</button>
+      <button class="next" id="nextSmooth">NEXT</button>
+    </div>
+  </div>`;
+  // this.instanceRef = document.querySelector(`.slider${this.carouselId}`);
+    // this.slider = document.querySelector(`.slider${this.carouselId}`);
+    // this.removeAllCarouselItems(slider);
+    // slider.insertAdjacentHTML('afterbegin', this.getAllElementsMarkup());
+  }
+
+  // removeAllCarouselItems(slider) {
+  //   while (slider.childNodes.length > 1) {
+  //     slider.removeChild(slider.firstChild);
+  //   }
+  // }
+
+  // drawButtons = () => {
+  //   // this.instanceRef = document.querySelector(`.slider${this.carouselId}`);
+  //   this.instanceRef.insertAdjacentHTML('beforeend', this.getButtonsMarkup());
+  //   if (this.settings.dimension === 'Y') {
+  //     this.instanceRef.querySelector('.next').classList.add('y');
+  //     this.instanceRef.querySelector('.prev').classList.add('y');
+  //   }
+  // }
+
+  // removeButtons() {
+  //   this.instanceRef.removeChild(this.instanceRef.lastChild);
+  // }
+
+  addClickListeners = () => {
+    document.querySelector(`.slider${this.carouselId}`).querySelector('#nextSmooth').addEventListener('click', this.scrollNext);
+    document.querySelector(`.slider${this.carouselId}`).querySelector('#prevSmooth').addEventListener('click', this.scrollPrevious);
+  }
+
+  transitionCompleted = () => {
+    this.translationComplete = true;
+  }
 
   scrollNext = () => {
-    this.scroll(-1);
-  };
+    if (this.translationComplete === true) {
+      this.translationComplete = false;
+      this.index -= 1;
+      if (this.index === -1) {
+        this.index = this.values.length - 1;
+      }
+      const outerIndex = (this.index) % this.values.length;
+      for (let i = 0; i < this.values.length; i += 1) {
+        const item = document.querySelector(`.slider${this.carouselId}`).querySelector('.activeContainer').getElementsByClassName('item')[i];
+        item.style.opacity = '1';
+        item.style.transform = `translate(${this.currTransl[i] + 200}px)`;
+        this.currTransl[i] += 200;
+      }
+      const outerItem = document.querySelector(`.slider${this.carouselId}`).getElementsByClassName('item')[outerIndex];
+      outerItem.style.transform = `translate(${this.currTransl[outerIndex] - 200 * (this.values.length)}px)`;
+      outerItem.style.opacity = '0';
+      this.currTransl[outerIndex] -= 200 * (this.values.length);
+    }
+  }
 
   scrollPrevious = () => {
-    this.scroll(1);
-  };
-
-  showAnimation = () => {
-    // const contentList = document.getElementById(this.settings.rootRefId).querySelectorAll('.content');
-    // const contentArray = [...contentList];
-    // contentArray.forEach((contentDiv) => {
-    //   contentDiv.classList.add('animate');
-    // });
-  };
-
-  formatIndexWithOffset = (indexWithOffset) => (indexWithOffset % this.values.length >= 0
-    ? indexWithOffset % this.values.length
-    : (indexWithOffset % this.values.length) + this.values.length);
-
-  isIndexItemActive = (index) => {
-    const lastLowInactive = (this.settings.visibleItems - this.settings.activeItems) / 2 - 1;
-    const firstHighInactive = (this.settings.visibleItems
-       - (this.settings.visibleItems - this.settings.activeItems) / 2);
-    return ((index > lastLowInactive) && (index < firstHighInactive));
-  };
-
-  getAllElementsMarkup = () => this.values
-    .reduce((elementsMarkup, element, index, valuesArray) => {
-      if (index < this.settings.visibleItems) {
-        return (this.isIndexItemActive(index))
-          ? `${elementsMarkup}<div class="content active">
-        <img src="${valuesArray[this.formatIndexWithOffset(index + this.offset)].picture}" alt="">
-        <h3>${valuesArray[this.formatIndexWithOffset(index + this.offset)].title}</h3>
-        <p>${valuesArray[this.formatIndexWithOffset(index + this.offset)].description}</p>
-      </div>`
-          : `${elementsMarkup}<div class="content">
-        <img src="${valuesArray[this.formatIndexWithOffset(index + this.offset)].picture}" alt="">
-        <h3>${valuesArray[this.formatIndexWithOffset(index + this.offset)].title}</h3>
-        <p>${valuesArray[this.formatIndexWithOffset(index + this.offset)].description}</p>
-      </div>`;
-      } return elementsMarkup;
-    }, '');
-
-    // getAllElementsMarkupWithAnimation = () => this.values
-    // .reduce((elementsMarkup, element, index, valuesArray) => {
-    //   if (index < this.settings.visibleItems) {
-    //     return (this.isIndexItemActive(index))
-    //       ? `${elementsMarkup}<div class="content active animate">
-    //     <img src="${valuesArray[this.formatIndexWithOffset(index + this.offset)].picture}" alt="">
-    //     <h3>${valuesArray[this.formatIndexWithOffset(index + this.offset)].title}</h3>
-    //     <p>${valuesArray[this.formatIndexWithOffset(index + this.offset)].description}</p>
-    //   </div>`
-    //       : `${elementsMarkup}<div class="content animate">
-    //     <img src="${valuesArray[this.formatIndexWithOffset(index + this.offset)].picture}" alt="">
-    //     <h3>${valuesArray[this.formatIndexWithOffset(index + this.offset)].title}</h3>
-    //     <p>${valuesArray[this.formatIndexWithOffset(index + this.offset)].description}</p>
-    //   </div>`;
-    //   } return elementsMarkup;
-    // }, '');
-
-  getButtonsMarkup = () => `<div class="buttons">
-    <button class="next">NEXT</button>
-    <button class="prev">PREV</button>
-  </div>`;
-
-  createSliderContainer() {
-    const slider = document.createElement('div');
-    slider.classList.add('slider');
-    slider.classList.add(`slider${this.carouselId}`);
-    if (this.settings.dimension === 'Y') {
-      slider.classList.add('slider-y');
-    } else {
-      slider.classList.add('slider-x');
+    if (this.translationComplete === true) {
+      this.translationComplete = false;
+      this.index += 1;
+      const outerIndex = (this.index - 1) % this.values.length;
+      for (let i = 0; i < this.values.length; i += 1) {
+        const item = document.querySelector(`.slider${this.carouselId}`).querySelector('.activeContainer').getElementsByClassName('item')[i];
+        item.style.opacity = '1';
+        item.style.transform = `translate(${this.currTransl[i] - 200}px)`;
+        this.currTransl[i] -= 200;
+      }
+      const outerItem = document.querySelector(`.slider${this.carouselId}`).getElementsByClassName('item')[outerIndex];
+      outerItem.style.opacity = '0';
+      outerItem.style.transform = `translate(${this.currTransl[outerIndex] + 200 * (this.values.length)}px)`;
+      this.currTransl[outerIndex] += 200 * (this.values.length);
     }
-    document.getElementById(this.settings.rootRefId).appendChild(slider);
-  }
-
-  updateSliderContainer(newOrientation) {
-    document.getElementById(this.settings.rootRefId).querySelector('.slider').classList.remove('slider-x', 'slider-y');
-    document.getElementById(this.settings.rootRefId).querySelector('.slider').classList.add(`slider-${newOrientation.toLowerCase()}`);
-  }
-
-  draw() {
-    const slider = document.querySelector(`.slider${this.carouselId}`);
-    this.removeAllCarouselItems(slider);
-    slider.insertAdjacentHTML('afterbegin', this.getAllElementsMarkup());
-  }
-
-  removeAllCarouselItems(slider) {
-    while (slider.childNodes.length > 1) {
-      slider.removeChild(slider.firstChild);
-    }
-  }
-
-  drawButtons() {
-    this.instanceRef = document.querySelector(`.slider${this.carouselId}`);
-    this.instanceRef.insertAdjacentHTML('beforeend', this.getButtonsMarkup());
-    if (this.settings.dimension === 'Y') {
-      this.instanceRef.querySelector('.next').classList.add('y');
-      this.instanceRef.querySelector('.prev').classList.add('y');
-    }
-  }
-
-  removeButtons() {
-    this.instanceRef.removeChild(this.instanceRef.lastChild);
-  }
-
-  addClickListeners() {
-    this.instanceRef.querySelector('.next').addEventListener('click', this.scrollNext);
-    this.instanceRef.querySelector('.prev').addEventListener('click', this.scrollPrevious);
   }
 
   addSwipeListeners = () => {
-    this.instanceRef.addEventListener('touchstart', touchStartHandle, false);
-    this.instanceRef.addEventListener('touchmove', touchMoveHandle, false);
+    document.querySelector(`.slider${this.carouselId}`).addEventListener('touchstart', touchStartHandle, false);
+    document.querySelector(`.slider${this.carouselId}`).addEventListener('touchmove', touchMoveHandle, false);
 
     let deltaX = null;
     let deltaY = null;
@@ -258,26 +339,28 @@ class Carousel{
       deltaX = null;
       deltaY = null;
     }
-  };
+  }
 }
 
-function parseValuesFromContainer(values){
-  try{
+function parseValuesFromContainer(values) {
+  try {
     const element = document.getElementById(values);
     const childDivs = Array.from(element.getElementsByTagName('div'));
 
-    return childDivs.reduce((valuesArray,currentDiv) => {
+    return childDivs.reduce((valuesArray, currentDiv) => {
       const picture = currentDiv.getElementsByTagName('img')[0].getAttribute('src');
       const title = currentDiv.getElementsByTagName('h3')[0].innerHTML;
       const description = currentDiv.getElementsByTagName('p')[0].innerHTML;
       element.innerHTML = '';
-      return [...valuesArray,{picture,title,description}];
-    },[])
-  } catch(error) {
-      console.log('We could not parse given template, try again');
-      console.log(`${error.name} : ${error.message}`);
-      throw error;
+      return [...valuesArray, { picture, title, description }];
+    }, []);
+  } catch (error) {
+    console.log('We could not parse given template, try again');
+    console.log(`${error.name} : ${error.message}`);
+    throw error;
   }
 }
+
+Carousel.id = 0;
 
 export { Carousel };
--- src/main.js
+++ src/main.js
@@ -21,6 +21,14 @@ const firstCarousel = new Carousel([{
   picture: 'https://placekitten.com/215/215',
   title: 'CAT #5',
   description: 'I wish i had a cat like this',
+}, {
+  picture: 'https://placekitten.com/215/215',
+  title: 'CAT #5',
+  description: 'I wish i had a cat like this',
+}, {
+  picture: 'https://placekitten.com/215/215',
+  title: 'CAT #5',
+  description: 'I wish i had a cat like this',
 }], {
   rootRefId: 'placeForCarousel',
   dimension: 'X',
@@ -30,39 +38,39 @@ const firstCarousel = new Carousel([{
   scrollPerClick: 1,
 });
 
-setTimeout(() => {
-  firstCarousel.setSettings('placeForCarousel', 'Y', 3, 1, 2000, 1);
-}, 3000);
+// setTimeout(() => {
+//   firstCarousel.setSettings('placeForCarousel', 'Y', 3, 1, 2000, 1);
+// }, 3000);
 
 
-const testCarousel2 = new Carousel([{
-  picture: 'https://placekitten.com/220/220',
-  title: 'CAT #1',
-  description: 'This cat is a nice one for sure ',
-}, {
-  picture: 'https://placekitten.com/202/202',
-  title: 'CAT #2',
-  description: 'Not bad at all',
-}, {
-  picture: 'https://placebear.com/205/205',
-  title: 'CAT #3',
-  description: 'Huge and dangerous',
-}, {
-  picture: 'https://placebear.com/208/208',
-  title: 'CAT #4',
-  description: 'Black and white, do u like it?',
-}, {
-  picture: 'https://placebear.com/215/215',
-  title: 'CAT #5',
-  description: 'I wish i had a cat like this',
-}], {
-  rootRefId: 'placeForCarousel2',
-  dimension: 'X',
-  visibleItems: 4,
-  activeItems: 2,
-  scrollSpeed: 1000,
-  scrollPerClick: 2,
-});
+// const testCarousel2 = new Carousel([{
+//   picture: 'https://placekitten.com/220/220',
+//   title: 'CAT #1',
+//   description: 'This cat is a nice one for sure ',
+// }, {
+//   picture: 'https://placekitten.com/202/202',
+//   title: 'CAT #2',
+//   description: 'Not bad at all',
+// }, {
+//   picture: 'https://placebear.com/205/205',
+//   title: 'CAT #3',
+//   description: 'Huge and dangerous',
+// }, {
+//   picture: 'https://placebear.com/208/208',
+//   title: 'CAT #4',
+//   description: 'Black and white, do u like it?',
+// }, {
+//   picture: 'https://placebear.com/215/215',
+//   title: 'CAT #5',
+//   description: 'I wish i had a cat like this',
+// }], {
+//   rootRefId: 'placeForCarousel2',
+//   dimension: 'X',
+//   visibleItems: 4,
+//   activeItems: 2,
+//   scrollSpeed: 1000,
+//   scrollPerClick: 2,
+// });
 
 // const failCarousel = new Carousel([{
 //   picture: 'https://placebear.com/220/220',
@@ -122,11 +130,11 @@ const testCarousel2 = new Carousel([{
 //   scrollPerClick: 5,
 // });
 
-const testCarousel4 = new Carousel('alreadyWithElementsFixed', {
-  rootRefId: 'placeForCarousel3',
-  visibleItems: 4,
-  activeItems: 2,
-});
+// const testCarousel4 = new Carousel('alreadyWithElementsFixed', {
+//   rootRefId: 'placeForCarousel3',
+//   visibleItems: 4,
+//   activeItems: 2,
+// });
 
 // const testCarousel3 = new Carousel('alreadyWithElements', {
 //   rootRefId: 'placeForCarousel3',
